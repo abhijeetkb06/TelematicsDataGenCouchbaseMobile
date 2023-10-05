e5a4165d8cb9f105b48804a353af9fd74c18a29e
abhijeetkb06 <106691047+abhijeetkb06@users.noreply.github.com>
2023-10-05 11:10:55 -0500

    Updated for config

--- src/main/java/org/couchbase/ConcurrencyConfig.java
+++ src/main/java/org/couchbase/ConcurrencyConfig.java
@@ -8,10 +8,10 @@ package org.couchbase;
 public class ConcurrencyConfig {
 
     // Executor thread pool config and producer consumer thread range
-    public static final int EXECUTOR_THREAD_POOL = 1;
+    public static final int EXECUTOR_THREAD_POOL = 10;
 
     public static final int CONSUMER_START_RANGE = 0;
-    public static final int CONSUMER_END_RANGE = 1;
+    public static final int CONSUMER_END_RANGE = 10;
 
     // Mock data generation parallelism
     public static final int MOCK_DATA_START_RANGE = 1;
